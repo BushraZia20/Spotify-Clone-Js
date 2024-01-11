6c358b8de99e0d22aa5d83bdddc5396af1df2d63
Bushra Zia <bushraz072@gmail.com>
2024-01-11 22:25:20 +0530

    left & right section

---
+++ script.js
@@ -0,0 +1,14 @@
+const playlistImageData = {
+    image1: "https://misc.scdn.co/liked-songs/liked-songs-300.png",
+    image2: "https://seed-mix-image.spotifycdn.com/v6/img/pop/5wPHD9nWphKcikUhKUzIdY/en/default",
+    image3: "https://seed-mix-image.spotifycdn.com/v6/img/house/4PVn1b2WnnXdq80C7uaZLZ/en/default",
+    image4: "https://seed-mix-image.spotifycdn.com/v6/img/dance_electronic/6lB8vOoI4DRrrVxXwuV19c/en/default",
+    image5: "https://i.scdn.co/image/ab67616d0000b2739bcee2cb961a04d2e5a29f5c",
+    image6: "https://i.scdn.co/image/ab67706c0000da84e1626ca4d433df16f0ce1e68",
+  };
+  
+
+  
+        
+        
+    
