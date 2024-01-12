34f705bd00c17762a3ad90ac71fa9b77e1009224
Bushra Zia <bushraz072@gmail.com>
2024-01-13 00:02:22 +0530

    Album cards

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const playlistImageData = {
+const playlistImageData = [
+  {
     image1: "https://misc.scdn.co/liked-songs/liked-songs-300.png",
+    playListName: "Liked Music",
+  },
+  {
     image2: "https://seed-mix-image.spotifycdn.com/v6/img/pop/5wPHD9nWphKcikUhKUzIdY/en/default",
+    playListName: "Pop Mix",
+  },
+  {
     image3: "https://seed-mix-image.spotifycdn.com/v6/img/house/4PVn1b2WnnXdq80C7uaZLZ/en/default",
+    playListName: "House Mix",
+  },
+  {
     image4: "https://seed-mix-image.spotifycdn.com/v6/img/dance_electronic/6lB8vOoI4DRrrVxXwuV19c/en/default",
+    playListName: "Dance/Electronic Mix",
+  },
+  {
     image5: "https://i.scdn.co/image/ab67616d0000b2739bcee2cb961a04d2e5a29f5c",
+    playListName: "Happy Mix",
+  },
+  {
     image6: "https://i.scdn.co/image/ab67706c0000da84e1626ca4d433df16f0ce1e68",
-  };
-  
+    playListName: "Workout Mix",
+  }
+]
 
-  
-        
-        
+
+
+
+
     
